2da0b8abc6a849864def59627da907d3cb0a440e
Kim Goulbourne <kimgoulbourne@Kims-MacBook-Pro.local>
2020-01-18 18:26:52 -0500

    Use craft request segment to determine which word to use in the intro

--- build/src/javascripts/app/components/page-intro.js
+++ build/src/javascripts/app/components/page-intro.js
@@ -23,28 +23,28 @@ export default class PageIntro {
     }
   }
 
-  _updatePageHeading() {
-    var query = this._queryConvert(window.location.search);
-    this.$pageHeading.text(this.$pageHeading[0].textContent.replace("business", query.q));
-    // console.log(query, this.$pageHeading[0].textContent, query.q);
-  }
+  // _updatePageHeading() {
+  //   var query = this._queryConvert(window.location.search);
+  //   this.$pageHeading.text(this.$pageHeading[0].textContent.replace("business", query.q));
+  //   // console.log(query, this.$pageHeading[0].textContent, query.q);
+  // }
 
-    // query string would normally be window.location.search
-  _queryConvert(query_string) {
-    var queryStr = query_string,
-      queryParams = [];
+  //   // query string would normally be window.location.search
+  // _queryConvert(query_string) {
+  //   var queryStr = query_string,
+  //     queryParams = [];
 
-    if (queryStr !== "") {
-      var queryArr = queryStr.replace('?', '').split('&');
+  //   if (queryStr !== "") {
+  //     var queryArr = queryStr.replace('?', '').split('&');
 
-      $.each(queryArr, function (q, query) {
-        var qArr = query.split('=');
-        queryParams[qArr[0]] = qArr[1];
-      });
-    }
+  //     $.each(queryArr, function (q, query) {
+  //       var qArr = query.split('=');
+  //       queryParams[qArr[0]] = qArr[1];
+  //     });
+  //   }
 
-    return queryParams;
-  }
+  //   return queryParams;
+  // }
 }
 
 new PageIntro();
