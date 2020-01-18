aa3bc245313e0582bad71285ac5b0bf5bc6679c7
Kim Goulbourne <kimgoulbourne@Kims-MacBook-Pro.local>
2020-01-18 18:01:07 -0500

    Update page heading based on the query in the url

--- build/src/javascripts/app/components/page-intro.js
+++ build/src/javascripts/app/components/page-intro.js
@@ -1,6 +1,7 @@
 export default class PageIntro {
   constructor() {
     this.$header = $('[data-header]');
+    this.$pageHeading = $('[data-page-heading]');
     this.$pageIntroCta = $('[data-page-intro-cta]');
 
     this._bindEvents();
@@ -8,6 +9,7 @@ export default class PageIntro {
 
   _bindEvents() {
     this.$pageIntroCta.on("click", this._scrollToForm.bind(this));
+    this._updatePageHeading();
   }
 
   _scrollToForm() {
@@ -19,6 +21,29 @@ export default class PageIntro {
         scrollTop: $(section).offset().top - self.$header.outerHeight() - 30
       });
     }
+  }
+
+  _updatePageHeading() {
+    var query = this._queryConvert(window.location.search);
+    this.$pageHeading.text(this.$pageHeading[0].textContent.replace("business", query.q));
+    // console.log(query, this.$pageHeading[0].textContent, query.q);
+  }
+
+    // query string would normally be window.location.search
+  _queryConvert(query_string) {
+    var queryStr = query_string,
+      queryParams = [];
+
+    if (queryStr !== "") {
+      var queryArr = queryStr.replace('?', '').split('&');
+
+      $.each(queryArr, function (q, query) {
+        var qArr = query.split('=');
+        queryParams[qArr[0]] = qArr[1];
+      });
+    }
+
+    return queryParams;
   }
 }
 
