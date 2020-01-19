6310c94ee157b8419bcd4c1de1615b3dea4301b8
Kim Goulbourne <kimgoulbourne@Kims-MacBook-Pro.local>
2020-01-18 19:01:53 -0500

    Add nav to landing page

--- build/src/javascripts/app/components/nav.js
+++ build/src/javascripts/app/components/nav.js
@@ -1,5 +1,6 @@
 export default class Navigation {
   constructor() {
+    this.$header = $('[data-header]');
     this.$nav = $('[data-nav]');
     this.$hamburger = $('[data-hamburger]');
 
@@ -7,12 +8,30 @@ export default class Navigation {
   }
 
   _bindEvents() {
+    let self = this;
     this.$hamburger.on("click", this._toggleNav.bind(this));
+
+    if (self.$nav.attr("data-nav") === "landing-page") {
+      self._scrollToSection();
+    }
   }
 
   _toggleNav() {
     this.$hamburger.toggleClass("is-active");
     this.$nav.toggleClass("is-open");
+  }
+
+  _scrollToSection(){
+    let self = this;
+    
+    $("[data-nav-item]").on("click", function(){
+      let section = $(this).attr("data-href");
+      console.log(section);
+      $("html,body").animate({
+        scrollTop: $(section).offset().top - self.$header.outerHeight() - 30
+      });
+      self._toggleNav();
+    });
   }
 }
 
